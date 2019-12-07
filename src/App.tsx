f96d94d9d1835650cd357b425c3fa79778c42345
jinho hong <tpdleps@gmail.com>
2019-12-08 02:48:26 +0900

    feat: 🎸 add font noto sans kr , add GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+import Header from "./layout/header";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: noto sans kr;
+    }
+    a {
+      text-decoration: none;
+      color: inherit
+    }
+`;
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <GlobalStyle />
+      <link
+        href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap"
+        rel="stylesheet"
+      ></link>
+      <Header />
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
