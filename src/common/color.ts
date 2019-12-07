9c9740d50170bd5742d6316c2b3f286a7c32d8c1
jinho hong <tpdleps@gmail.com>
2019-12-08 03:59:43 +0900

    fix: 🐛 fix color error

--- src/common/color.ts
+++ src/common/color.ts
@@ -1,5 +1,6 @@
 export const colors = {
   GRAY: "rgba(0, 0, 0, 0.5)",
   TREVARI_ORANGE: "rgba(255, 137, 6, 1)",
-  BLACK: "rgb(225, 225, 225, 1)"
+  BLACK: "rgba(0, 0, 0, 1)",
+  WHITE: "rgba(255, 255, 255, 1)"
 };
