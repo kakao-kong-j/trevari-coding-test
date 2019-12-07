84d1824b4b40ede883bfa5964614acee4733fa9a
jinho hong <tpdleps@gmail.com>
2019-12-08 04:00:24 +0900

    feat: 🎸 add badge components

---
+++ src/components/badge/index.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+import { BadgeStyle } from "./style";
+
+export const Badge: FC = () => {
+  return <BadgeStyle>OPEN</BadgeStyle>;
+};
--- src/components/serviceNavigatorElement/index.tsx
+++ src/components/serviceNavigatorElement/index.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from "react";
 import { Category } from "../serviceNavigator/types";
 import { ServiceNavigatorElementStyle } from "./style";
+import { Badge } from "../badge";
 
 export const ServiceNavigatorElement: FC<Category> = props => {
-  const { name, uri } = props;
+  const { name, uri, isOpen } = props;
   return (
     <ServiceNavigatorElementStyle>
       <a href={uri}>
         <span>{name}</span>
+        {isOpen && <Badge>OPEN</Badge>}
       </a>
     </ServiceNavigatorElementStyle>
   );
