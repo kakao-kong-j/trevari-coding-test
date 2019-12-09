1138757973b48e2c7dbbb0ed17f5ece8ab0ea138
jinho hong <tpdleps@gmail.com>
2019-12-09 22:30:47 +0900

    feat: 🎸 add PrimaryButton

---
+++ src/components/primaryButton/style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { colors } from "../../common/color";
+
+export const PrimaryButtonStyle = styled.button`
+  font-weight: 500;
+  text-align: center;
+  background-color: ${colors.TREVARI_ORANGE};
+  color: ${colors.WHITE};
+  padding: 12px 52px;
+  font-weight: 400;
+  font-size: 110%;
+  margin-bottom: 16px;
+`;
