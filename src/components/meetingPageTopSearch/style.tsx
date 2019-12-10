15723244bd92f71d4de5a2312549bb241c146e25
jinho hong <tpdleps@gmail.com>
2019-12-10 17:41:47 +0900

    feat: 🎸 add MeetingPageCalenderButtonStyle

--- src/components/meetingPageTopSearch/style.tsx
+++ src/components/meetingPageTopSearch/style.tsx
@@ -1,3 +1,14 @@
 import styled from "styled-components";
+import { PrimaryButtonStyle } from "../primaryButton/style";
 
-export const MeetingPageTopSearchStyle = styled.div``;
+export const MeetingPageTopSearchStyle = styled.div`
+  overflow: hidden;
+  padding-top: 32px;
+  padding: 0 16px;
+`;
+
+export const MeetingPageCalenderButtonStyle = styled(PrimaryButtonStyle)`
+  margin: 8px 16px;
+  border-radius: 0.125rem;
+  border: 1px solid transparent;
+`;
