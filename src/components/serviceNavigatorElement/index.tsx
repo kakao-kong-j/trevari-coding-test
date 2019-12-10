7638dacf505415f75ce0e0b76b0b77e321e352a4
jinho hong <tpdleps@gmail.com>
2019-12-10 20:14:13 +0900

    fix: 🐛 anchor to react router Link

--- src/components/serviceNavigatorElement/index.tsx
+++ src/components/serviceNavigatorElement/index.tsx
@@ -2,15 +2,16 @@ import React, { FC } from "react";
 import { Category } from "../serviceNavigator/types";
 import { ServiceNavigatorElementStyle } from "./style";
 import { Badge } from "../badge";
+import { Link } from "react-router-dom";
 
 export const ServiceNavigatorElement: FC<Category> = props => {
   const { name, uri, isOpen } = props;
   return (
     <ServiceNavigatorElementStyle>
-      <a href={uri}>
+      <Link to={uri}>
         <span>{name}</span>
         {isOpen && <Badge>OPEN</Badge>}
-      </a>
+      </Link>
     </ServiceNavigatorElementStyle>
   );
 };
