52e30285d2b19d9298e30c5bbf5ae4d85ec928d6
jinho hong <tpdleps@gmail.com>
2019-12-10 14:11:21 +0900

    feat: 🎸 MeetingCard width error fix

--- src/components/meetingCard/style.tsx
+++ src/components/meetingCard/style.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MeetingCardStyle = styled.div`
   margin: 0px 8px 32px;
-  width: 100%;
+  width: 30%;
 `;
 
 export const MeetingCardAnchorStyle = styled.a``;
