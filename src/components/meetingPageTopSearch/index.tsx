4071520e7fef66b81077cfb56b465fbe81bdc9b0
jinho hong <tpdleps@gmail.com>
2019-12-10 17:42:39 +0900

    feat: 🎸 add button style && add description

--- src/components/meetingPageTopSearch/index.tsx
+++ src/components/meetingPageTopSearch/index.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react";
-import { MeetingPageTopSearchStyle } from "./style";
-import { PrimaryButton } from "../primaryButton";
+import {
+  MeetingPageTopSearchStyle,
+  MeetingPageCalenderButtonStyle
+} from "./style";
 import { SearchInput } from "../searchInput";
+import { colors } from "../../common/color";
 
 export const MeetingPageTopSearch: FC = () => {
   return (
     <MeetingPageTopSearchStyle>
-      <PrimaryButton value="독서모임 캘린더 바로가기" />
+      <div style={{ color: colors.GRAY_123 }}>
+        내 일정에 맞는 놀러가기 클럽을 찾으신다면?
+      </div>
+      <MeetingPageCalenderButtonStyle>
+        독서모임 캘린더 바로가기
+      </MeetingPageCalenderButtonStyle>
       <SearchInput buttonValue="검색" placeholder="클럽명 검색" />
     </MeetingPageTopSearchStyle>
   );
