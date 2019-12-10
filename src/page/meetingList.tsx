f41b43cdd17da460698ac534dd6f4f09980323aa
jinho hong <tpdleps@gmail.com>
2019-12-11 07:07:56 +0900

    feat: 🎸 [WIP] add infinityScroll

--- src/page/meetingList.tsx
+++ src/page/meetingList.tsx
@@ -1,10 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useState, useEffect } from "react";
 import { MeetingPageTopSearch } from "../components/meetingPageTopSearch";
 import { MeetingCardGroup } from "../components/meetingCardGroup";
-import { useMeetingState } from "../context/meeting";
+import { useMeetingState, useMeetingDispatch } from "../context/meeting";
+import { fetchMeetings } from "../hook/fetchMeetings";
+import { useScroll } from "../hook/useScroll";
 
 export const MeetingListPage: FC = () => {
   const meetings = useMeetingState();
+
+  const [loadMore, setLoadMore] = useState(true);
+  const { height } = useScroll();
+
+  const getData = async () => {
+    const fetchedMeetings = await fetchMeetings();
+    dispatch({ type: "BATCH_APPEND", meetings: fetchedMeetings });
+  };
+
+  useEffect(() => {
+    getData();
+  }, [loadMore]);
+
+  const dispatch = useMeetingDispatch();
+
+  useEffect(() => {
+    if (height * 1.3 > window.scrollY) {
+      getData();
+      setLoadMore(true);
+    }
+  });
+
   return (
     <div style={{ maxWidth: "1140px", margin: "0 auto", marginTop: "20px" }}>
       <MeetingPageTopSearch />
