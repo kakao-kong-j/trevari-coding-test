53095df180d37ed1435be5bcd3bc58e3adb1817f
jinho hong <tpdleps@gmail.com>
2019-12-10 12:00:24 +0900

    refactor: 💡 split MeetingCardDetailSection

--- src/components/meetingCard/index.tsx
+++ src/components/meetingCard/index.tsx
@@ -12,17 +12,10 @@ import {
 import { Meeting, MeetingType } from "./types";
 import { convertLocalTime } from "../../common/time";
 
-export const MeetingCardImageSection: FC<{ meeting: Meeting }> = props => {
-  const {
-    clubID,
-    contents,
-    coverUrl,
-    leader,
-    name,
-    place,
-    startedAt,
-    type
-  } = props.meeting;
+export const MeetingCardImageSection: FC<{
+  meeting: Pick<Meeting, "coverUrl" | "type" | "leader">;
+}> = props => {
+  const { coverUrl, leader, type } = props.meeting;
   return (
     <MeetingCardWarpperStyle>
       <MeetingCardCoverImageStyle src={coverUrl} />
@@ -37,33 +30,33 @@ export const MeetingCardImageSection: FC<{ meeting: Meeting }> = props => {
   );
 };
 
+export const MeetingCardDetailSection: FC<{
+  meeting: Pick<Meeting, "contents" | "place" | "startedAt">;
+}> = props => {
+  const { contents, place, startedAt } = props.meeting;
+  return (
+    <div style={{ margin: "8px 0px" }}>
+      <MeetingCardNameStyle>{name}</MeetingCardNameStyle>
+      <MeetingCardContentsStyle>
+        {contents.length === 1
+          ? `${contents[0].author}, 『${contents[0].title}』`
+          : `${contents[0].author}, 『${contents[0].title}』 외 ${contents.length}개의 읽을거리`}
+      </MeetingCardContentsStyle>
+      <MeetingCardDescriptionStyle>
+        <div>{place.name}</div>
+        <div>{convertLocalTime(startedAt)}</div>
+      </MeetingCardDescriptionStyle>
+    </div>
+  );
+};
+
 export const MeetingCard: FC<{ meeting: Meeting }> = props => {
-  const {
-    clubID,
-    contents,
-    coverUrl,
-    leader,
-    name,
-    place,
-    startedAt,
-    type
-  } = props.meeting;
+  const { clubID } = props.meeting;
   return (
     <MeetingCardStyle key={clubID}>
       <MeetingCardAnchorStyle>
         <MeetingCardImageSection meeting={props.meeting} />
-        <div style={{ margin: "8px 0px" }}>
-          <MeetingCardNameStyle>{name}</MeetingCardNameStyle>
-          <MeetingCardContentsStyle>
-            {contents.length === 1
-              ? `${contents[0].author}, 『${contents[0].title}』`
-              : `${contents[0].author}, 『${contents[0].title}』 외 ${contents.length}개의 읽을거리`}
-          </MeetingCardContentsStyle>
-          <MeetingCardDescriptionStyle>
-            <div>{place.name}</div>
-            <div>{convertLocalTime(startedAt)}</div>
-          </MeetingCardDescriptionStyle>
-        </div>
+        <MeetingCardDetailSection meeting={props.meeting} />
       </MeetingCardAnchorStyle>
     </MeetingCardStyle>
   );
