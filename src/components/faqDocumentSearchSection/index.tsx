a5f709ac0e79a69d23d40fc31f112f4d302e0334
Hong jinho <tpdleps@gmail.com>
2019-12-11 09:57:02 +0900

    feat: 🎸 add faq search feature (#27)
    * feat: 🎸 add faq search feature
    
    * feat: 🎸 remove warning

--- src/components/faqDocumentSearchSection/index.tsx
+++ src/components/faqDocumentSearchSection/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import {
   FaqDocumentSearchSelectBoxStyle,
   FaqDocumentSearchInputStyle,
@@ -6,18 +6,46 @@ import {
   FaqDocumentSearchButtonStyle
 } from "./style";
 import { useInput } from "../../hook/useInput";
+import { useFaqDispatch } from "../../context/faq";
+import { fetchFaqs } from "../../common/fetchFaqs";
 
 export const FaqDocumentSearchSection: FC = () => {
   const input = useInput("");
+  const [currentOption, setOption] = useState("제목&내용");
+  const [currentInput, setInput] = useState("");
+
+  const dispatch = useFaqDispatch();
+
+  const getFaq = async () => {
+    const fetchedFaqs = await fetchFaqs();
+    dispatch({ type: "BATCH_APPEND", faqs: fetchedFaqs });
+    dispatch({ type: "SEARCH", text: input.value, option: currentOption });
+  };
+
+  useEffect(() => {
+    dispatch({ type: "CLEAR" });
+    getFaq();
+  }, [currentInput, currentOption]);
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setOption(e.target.value);
+  };
+
+  const onClick = () => {
+    setInput(input.value);
+  };
+
   return (
     <FaqDocumentSearchSectionStyle>
-      <FaqDocumentSearchSelectBoxStyle>
-        <option>{"제목&내용"}</option>
-        <option>제목</option>
-        <option>내용</option>
+      <FaqDocumentSearchSelectBoxStyle onChange={onChange}>
+        <option value="제목&내용">{"제목&내용"}</option>
+        <option value="제목">제목</option>
+        <option value="내용">내용</option>
       </FaqDocumentSearchSelectBoxStyle>
       <FaqDocumentSearchInputStyle {...input}></FaqDocumentSearchInputStyle>
-      <FaqDocumentSearchButtonStyle>검색</FaqDocumentSearchButtonStyle>
+      <FaqDocumentSearchButtonStyle onClick={onClick}>
+        검색
+      </FaqDocumentSearchButtonStyle>
     </FaqDocumentSearchSectionStyle>
   );
 };
